d4c9317c6e89f31c6f1c196826819662b84f17c7
Karmakos <johnthewriter2020@gmail.com>
2023-05-21 21:51:59 +0300

    Decluttering
    Decluttering old files in the Ecommerce webapp

---
+++ src/Header.js
@@ -0,0 +1,53 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './Header.css';
+
+function Header() {
+  return (
+    <div>
+      <header>
+      <div container main>
+      <div row-1>
+      <div class="container">
+      <div class="row">
+      <div class="col">
+      <p>Welcome to Karmatt store. We value you</p>
+      </div>
+      <div class="col topbar-right">
+      <ul>
+      <li class="d-inline">
+        <a href="#">Store Locator</a>
+      </li>
+      <li class="d-inline">
+        <a href="#">Track Your Order</a>
+      </li>
+      <li class="d-inline">
+        <a href="#">Register <span>or</span> Sign in</a>
+      </li>
+      </ul>
+      </div>
+
+      </div>
+
+
+
+      </div>
+      
+
+
+      </div>
+
+
+
+
+
+
+
+
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
